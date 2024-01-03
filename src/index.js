d48d3f30152083ce4ba694ad4be4dc7198909443
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-01-03 17:10:12 +0800

    removed icons and created node module

---
+++ src/index.js
@@ -0,0 +1,151 @@
+/* Buttons & Links */
+import UButton from "./ui.button";
+import UButtonGroup from "./ui.button-group";
+import UButtonGroupItem from "./ui.button-group-item";
+import UButtonExpand from "./ui.button-expand";
+import UButtonExpandItem from "./ui.button-expand-item";
+import ULink from "./ui.button-link";
+
+/* Dropdowns */
+import UDropdownButton from "./ui.dropdown-button";
+import UDropdownTag from "./ui.dropdown-tag";
+import UDropdownLink from "./ui.dropdown-link";
+import UDropdownItem from "./ui.dropdown-item";
+
+/* Form Inputs & Controls */
+import UInput from "./ui.form-input";
+import UInputFile from "./ui.form-input-file";
+import UInputMoney from "./ui.form-input-money";
+import UInputSearch from "./ui.form-input-search";
+import UInputNumber from "./ui.form-input-number";
+import UInputRating from "./ui.form-input-rating";
+import UTextarea from "./ui.form-textarea";
+import USelect from "./ui.form-select";
+import UMultiselect from "./ui.form-select-multi";
+import UCheckbox from "./ui.form-checkbox";
+import UCheckboxGroup from "./ui.form-checkbox-group";
+import UCheckboxMultiState from "./ui.form-checkbox-multi-state";
+import USwitch from "./ui.form-switch";
+import URadio from "./ui.form-radio";
+import URadioCard from "./ui.form-radio-card";
+import URadioGroup from "./ui.form-radio-group";
+import UDatePicker from "./ui.form-date-picker";
+import UDatePickerRange from "./ui.form-date-picker-range";
+
+/* Text & Content */
+import UHeader from "./ui.text-header";
+import UText from "./ui.text-block";
+import UAlert from "./ui.text-alert";
+import UMoney from "./ui.text-money";
+import UFile from "./ui.text-file";
+import UFiles from "./ui.text-files";
+import UField from "./ui.text-field";
+import UEmpty from "./ui.text-empty";
+import UTag from "./ui.text-tag";
+
+/* Containers */
+import UDivider from "./ui.container-divider";
+import URow from "./ui.container-row";
+import UGroup from "./ui.container-group";
+import UGroups from "./ui.container-groups";
+import UAccordion from "./ui.container-accordion";
+import UCard from "./ui.container-card";
+import UModal from "./ui.container-modal";
+import UModalConfirm from "./ui.container-modal-confirm";
+import UPage from "./ui.container-page";
+
+/* Images and Icons */
+import UIcon from "./ui.image-icon";
+import ULogo from "./ui.image-logo";
+import UAvatar from "./ui.image-avatar";
+
+/* Data */
+import UTable from "./ui.data-table";
+import UDataList from "./ui.data-list";
+
+/* Navigation */
+import UTab from "./ui.navigation-tab";
+import UTabs from "./ui.navigation-tabs";
+import UStepper from "./ui.navigation-stepper";
+import UPagination from "./ui.navigation-pagination";
+
+/* Other */
+import UDot from "./ui.other-dot";
+import ULoader from "./ui.other-loader";
+
+export {
+  /* Buttons & Links */
+  UButton,
+  UButtonGroup,
+  UButtonGroupItem,
+  UButtonExpand,
+  UButtonExpandItem,
+  ULink,
+
+  /* Dropdowns */
+  UDropdownButton,
+  UDropdownTag,
+  UDropdownLink,
+  UDropdownItem,
+
+  /* Form Inputs & Controls */
+  UInput,
+  UInputFile,
+  UInputMoney,
+  UInputSearch,
+  UInputNumber,
+  UInputRating,
+  UTextarea,
+  USelect,
+  UMultiselect,
+  UCheckbox,
+  UCheckboxGroup,
+  UCheckboxMultiState,
+  USwitch,
+  URadio,
+  URadioCard,
+  URadioGroup,
+  UDatePicker,
+  UDatePickerRange,
+
+  /* Text & Content */
+  UHeader,
+  UText,
+  UAlert,
+  UMoney,
+  UFile,
+  UFiles,
+  UField,
+  UEmpty,
+  UTag,
+
+  /* Containers */
+  UDivider,
+  URow,
+  UGroup,
+  UGroups,
+  UAccordion,
+  UCard,
+  UModal,
+  UModalConfirm,
+  UPage,
+
+  /* Images and Icons */
+  UIcon,
+  ULogo,
+  UAvatar,
+
+  /* Data */
+  UTable,
+  UDataList,
+
+  /* Navigation */
+  UTab,
+  UTabs,
+  UStepper,
+  UPagination,
+
+  /* Other */
+  UDot,
+  ULoader,
+};
--- src/ui.icons-metronic/index.js
+++
@@ -1,4 +0,0 @@
-// Import icons directly
-//
-// require("vueless/ui.icons-metronic/icon-name.svg")
-//
