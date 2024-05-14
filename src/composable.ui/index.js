e5acfbf06e082796320b2c99986abba70288ead2
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-05-14 23:20:17 +0800

    Moved cloneDeep to separate helper service.

--- src/composable.ui/index.js
+++ src/composable.ui/index.js
@@ -9,6 +9,7 @@ import {
   Fragment,
 } from "vue";
 import { cx, globalComponentConfig, strategy } from "../service.ui";
+import { cloneDeep } from "../service.helper";
 
 const strategyType = {
   merge: "merge",
@@ -343,51 +344,6 @@ function hasSlotContent(slot, props = {}) {
   };
 
   return !isVNodeEmpty(slot?.(props));
-}
-
-/**
- Deeply clone given object (same as lodash.cloneDeep).
- @param {Object} entity
- @param cache
-
- @returns {Object}
- */
-function cloneDeep(entity, cache = new WeakMap()) {
-  const referenceTypes = ["Array", "Object", "Map", "Set", "Date"];
-  const entityType = Object.prototype.toString.call(entity);
-
-  if (
-    !new RegExp(referenceTypes.join("|")).test(entityType) ||
-    entity instanceof WeakMap ||
-    entity instanceof WeakSet
-  ) {
-    return entity;
-  }
-
-  if (cache.has(entity)) {
-    return cache.get(entity);
-  }
-
-  const c = new entity.constructor();
-
-  if (entity instanceof Map) {
-    entity.forEach((value, key) => c.set(cloneDeep(key), cloneDeep(value)));
-  }
-
-  if (entity instanceof Set) {
-    entity.forEach((value) => c.add(cloneDeep(value)));
-  }
-
-  if (entity instanceof Date) {
-    return new Date(entity);
-  }
-
-  cache.set(entity, c);
-
-  return Object.assign(
-    c,
-    ...Object.keys(entity).map((prop) => ({ [prop]: cloneDeep(entity[prop], cache) })),
-  );
 }
 
 function setColor(classes, color) {
---
+++ src/service.helper/index.js
@@ -0,0 +1,44 @@
+/**
+ Deeply clone given object (same as lodash.cloneDeep).
+ @param {Object} entity
+ @param cache
+
+ @returns {Object}
+ */
+export function cloneDeep(entity, cache = new WeakMap()) {
+  const referenceTypes = ["Array", "Object", "Map", "Set", "Date"];
+  const entityType = Object.prototype.toString.call(entity);
+
+  if (
+    !new RegExp(referenceTypes.join("|")).test(entityType) ||
+    entity instanceof WeakMap ||
+    entity instanceof WeakSet
+  ) {
+    return entity;
+  }
+
+  if (cache.has(entity)) {
+    return cache.get(entity);
+  }
+
+  const c = new entity.constructor();
+
+  if (entity instanceof Map) {
+    entity.forEach((value, key) => c.set(cloneDeep(key), cloneDeep(value)));
+  }
+
+  if (entity instanceof Set) {
+    entity.forEach((value) => c.add(cloneDeep(value)));
+  }
+
+  if (entity instanceof Date) {
+    return new Date(entity);
+  }
+
+  cache.set(entity, c);
+
+  return Object.assign(
+    c,
+    ...Object.keys(entity).map((prop) => ({ [prop]: cloneDeep(entity[prop], cache) })),
+  );
+}
