46517f3bee593a366e77241d95da8e0a8e68a641
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-05-26 01:36:07 +0800

    Removed VuelessUnpluginComponents to prevent missing component import in the library. Removed vueless alias.

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,17 @@
 import { defineConfig } from "vite";
-import path from "path";
+
 // Plugins
 import Vue from "@vitejs/plugin-vue";
 import Eslint from "vite-plugin-eslint";
-import { Vueless, VuelessUnpluginComponents } from "@vueless/plugin-vite";
+import { Vueless } from "@vueless/plugin-vite";
 
 export default defineConfig({
-  plugins: [
-    Vue(),
-    Eslint(),
-    Vueless({ mode: "storybook", env: "vueless" }),
-    VuelessUnpluginComponents(),
-  ],
+  plugins: [Vue(), Eslint(), Vueless({ mode: "storybook", env: "vueless" })],
   resolve: {
     extensions: [".vue", ".mjs", ".js", ".ts", ".mdx"],
-    alias: {
-      vueless: path.resolve(__dirname, "./src"),
-    },
   },
   optimizeDeps: {
     include: [
-      "vuex",
-      "vue-tippy",
-      "@kyvg/vue3-notification",
       "@tailwindcss/forms",
       "tailwindcss/colors.js",
       "cva",
