6ee4f9cb0bd5e37d8dc53b4275e2237c4916f40a
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-05-30 13:45:02 +0800

    UProgress: documented props and changed default color to brand.

--- src/ui.navigation-progress/configs/default.config.js
+++ src/ui.navigation-progress/configs/default.config.js
@@ -1,13 +1,13 @@
 export default /*tw*/ {
   progress: {
     base: `
-      block appearance-none border-none overflow-hidden w-full 
-      [&::-webkit-progress-bar]:w-full h-2 rounded-full [&::-webkit-progress-bar]:rounded-full 
-      [&::-webkit-progress-bar]:bg-{color}-200 [@supports(selector(&::-moz-progress-bar))]:bg-{color}-200 
-      [&::-webkit-progress-value]:rounded-full  [&::-webkit-progress-value]:transition-all 
+      block appearance-none border-none overflow-hidden w-full
+      [&::-webkit-progress-bar]:w-full h-2 rounded-full [&::-webkit-progress-bar]:rounded-full
+      [&::-webkit-progress-bar]:bg-{color}-200 [@supports(selector(&::-moz-progress-bar))]:bg-{color}-200
+      [&::-webkit-progress-value]:rounded-full  [&::-webkit-progress-value]:transition-all
       [&::-webkit-progress-value]:ease-in-out [&::-moz-progress-bar]:rounded-full text-{color}-500
-      [&::-webkit-progress-value]:bg-current [&::-moz-progress-bar]:bg-current indeterminate:relative 
-      indeterminate:after:rounded-full [&:indeterminate::-webkit-progress-value]:rounded-full 
+      [&::-webkit-progress-value]:bg-current [&::-moz-progress-bar]:bg-current indeterminate:relative
+      indeterminate:after:rounded-full [&:indeterminate::-webkit-progress-value]:rounded-full
       [&:indeterminate::-moz-progress-bar]:rounded-full
     `,
     variants: {
@@ -27,7 +27,7 @@ export default /*tw*/ {
     },
   },
   defaultVariants: {
-    color: "green",
+    color: "brand",
     size: "md",
   },
   safelist: (colors) => [{ pattern: `bg-(${colors})-200` }, { pattern: `text-(${colors})-500` }],
--- src/ui.navigation-progress/index.vue
+++ src/ui.navigation-progress/index.vue
@@ -13,19 +13,36 @@ import { useAttrs } from "./composables/attrs.composable";
 defineOptions({ name: "UProgress", inheritAttrs: false });
 
 const props = defineProps({
+  /**
+   * Progress value (current step).
+   */
   value: {
     type: Number,
     required: true,
     default: 0,
   },
+
+  /**
+   * Progress max amount of steps.
+   */
   max: {
     type: Number,
     default: 100,
   },
+
+  /**
+   * Progress size.
+   * @values 2xs, xs, sm, md, lg, xl, 2xl
+   */
   size: {
     type: String,
     default: UIService.get(defaultConfig, UProgress).default.size,
   },
+
+  /**
+   * Progress color.
+   * @values brand, grayscale, gray, red, orange, amber, yellow, lime, green, emerald, teal, cyan, sky, blue, indigo, violet, purple, fuchsia, pink, rose, white
+   */
   color: {
     type: String,
     default: UIService.get(defaultConfig, UProgress).default.color,
