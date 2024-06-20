b75cb42b933a16528d7b4eb2907c91ea46f3675a
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-06-20 10:02:56 +0800

    Added web-types key to package.json for the dev purpose and mechanism of removing it from the dist.

---
+++ prepare.package.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+
+/* Web-types needs only for the dev purpose, so it needs to be removed from the dist. */
+removeWebTypesFromPackageJson();
+
+function removeWebTypesFromPackageJson() {
+  try {
+    const packageJsonPath = path.resolve(process.cwd(), "dist/package.json");
+    const data = fs.readFileSync(packageJsonPath, "utf8");
+    let packageJson = JSON.parse(data);
+
+    delete packageJson["web-types"];
+
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + "\n", "utf8");
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Error:", error);
+  }
+}
