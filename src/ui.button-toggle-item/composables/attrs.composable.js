c6d44c4dea029b0d9a17de9812c3e9f8b3a13bb4
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-07-12 23:57:15 +0300

    Update UToggleItem default config, adjust composable function

--- src/ui.button-toggle-item/composables/attrs.composable.js
+++ src/ui.button-toggle-item/composables/attrs.composable.js
@@ -1,32 +1,59 @@
-import { computed } from "vue";
+import { computed, toValue } from "vue";
 import useUI from "../../composable.ui";
-import { cva } from "../../service.ui";
+import { cx, cva } from "../../service.ui";
 import defaultConfig from "../configs/default.config";
 
-export default function useAttrs(props) {
-  const { config, getAttrs, hasSlotContent, isSystemKey } = useUI(
+export default function useAttrs(props, { selectedValue, separated, variant }) {
+  const { config, getAttrs, isSystemKey, hasSlotContent } = useUI(
     defaultConfig,
     () => props.config,
   );
+
+  const skipKeys = ["selected"];
+
   const attrs = {};
 
   for (const key in defaultConfig) {
-    if (isSystemKey(key)) continue;
+    if (isSystemKey(key) || skipKeys.includes(key)) continue;
 
-    let classes = "";
-    let value = config.value[key];
+    const classes = computed(() => {
+      const value = config.value[key];
 
-    if (value.variants || value.compoundVariants) {
-      const getCVA = cva(value);
-
-      classes = computed(() =>
-        getCVA({
+      if (value.variants || value.compoundVariants) {
+        return cva(value)({
           ...props,
-        }),
-      );
-    }
+          variant: toValue(variant),
+          separated: toValue(separated),
+        });
+      }
+
+      return "";
+    });
 
     attrs[`${key}Attrs`] = getAttrs(key, { classes });
+
+    if (key === "button") {
+      const selectedClasses = computed(() =>
+        cva(config.value.selected)({
+          ...props,
+          variant: toValue(variant),
+          separated: toValue(separated),
+        }),
+      );
+
+      const buttonAttrs = attrs[`${key}Attrs`];
+
+      attrs[`${key}Attrs`] = computed(() => {
+        const isSelected = Array.isArray(selectedValue?.value)
+          ? selectedValue?.value?.includes(props.value)
+          : selectedValue?.value === props.value;
+
+        return {
+          ...buttonAttrs.value,
+          class: cx([buttonAttrs.value.class, isSelected && selectedClasses.value]),
+        };
+      });
+    }
   }
 
   return {
