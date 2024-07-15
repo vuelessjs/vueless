02c7a7f2ba0f49ea7e03bdf2a93a070833395113
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-07-16 00:56:51 +0800

    Added dynamic attrs.

--- src/ui.button/composables/attrs.composable.js
+++ src/ui.button/composables/attrs.composable.js
@@ -1,58 +1,40 @@
 import { computed } from "vue";
 import useUI from "../../composable.ui";
 import { cva } from "../../service.ui";
-
 import defaultConfig from "../configs/default.config";
 
-export function useAttrs(props) {
-  const { config, getAttrs, getColor, setColor } = useUI(defaultConfig, () => props.config);
-  const { button, text } = config.value;
-
-  const cvaButton = cva({
-    base: button.base,
-    variants: button.variants,
-    compoundVariants: button.compoundVariants,
-  });
-
-  const cvaText = cva({
-    base: text.base,
-    variants: text.variants,
-    compoundVariants: text.compoundVariants,
-  });
-
-  const buttonClasses = computed(() =>
-    setColor(
-      cvaButton({
-        size: props.size,
-        color: getColor(props.color),
-        variant: props.variant,
-        pill: props.pill,
-        block: props.block,
-        square: props.loading || props.square,
-        filled: props.filled,
-        loading: props.loading,
-        disabled: props.disabled,
-      }),
-      props.color,
-    ),
+export default function useAttrs(props) {
+  const { config, getAttrs, hasSlotContent, isSystemKey, getColor, setColor } = useUI(
+    defaultConfig,
+    () => props.config,
   );
+  const attrs = {};
 
-  const textClasses = computed(() =>
-    setColor(
-      cvaText({
-        color: getColor(props.color),
-      }),
-      props.color,
-    ),
-  );
+  for (const key in defaultConfig) {
+    if (isSystemKey(key)) continue;
 
-  const buttonAttrs = getAttrs("button", { classes: buttonClasses });
-  const loaderAttrs = getAttrs("loader");
-  const textAttrs = getAttrs("text", { classes: textClasses });
+    const classes = computed(() => {
+      const value = config.value[key];
+
+      if (value.variants || value.compoundVariants) {
+        return setColor(
+          cva(value)({
+            ...props,
+            color: getColor(props.color),
+          }),
+          props.color,
+        );
+      }
+
+      return "";
+    });
+
+    attrs[`${key}Attrs`] = getAttrs(key, { classes });
+  }
 
   return {
-    buttonAttrs,
-    loaderAttrs,
-    textAttrs,
+    ...attrs,
+    config,
+    hasSlotContent,
   };
 }
