a256f03948e7e3fc02d9fc5b3101ec95b4e91c67
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-07-17 16:28:11 +0300

    Update UHeader's composable, add slotsFragment

--- src/ui.text-header/index.vue
+++ src/ui.text-header/index.vue
@@ -13,7 +13,7 @@ import UIService from "../service.ui";
 
 import { UHeader } from "./constants";
 import defaultConfig from "./configs/default.config";
-import { useAttrs } from "./composables/attrs.composable";
+import useAttrs from "./composables/attrs.composable";
 
 /* Should be a string for correct web-types gen */
 defineOptions({ name: "UHeader", inheritAttrs: false });
