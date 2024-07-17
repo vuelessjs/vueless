5355050b3b411ae82d626dfe0cd0f5d091bdb7e8
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-07-17 16:24:03 +0300

    Update UAlert's composable

--- src/ui.text-alert/composables/attrs.composable.js
+++ src/ui.text-alert/composables/attrs.composable.js
@@ -4,54 +4,38 @@ import { cva } from "../../service.ui";
 import defaultConfig from "../configs/default.config";
 import { computed } from "vue";
 
-export function useAttrs(props) {
-  const { config, getAttrs, hasSlotContent, getColor, setColor } = useUI(
+export default function useAttrs(props) {
+  const { config, getAttrs, hasSlotContent, getColor, setColor, isSystemKey } = useUI(
     defaultConfig,
     () => props.config,
   );
-  const { wrapper, body } = config.value;
+  const attrs = {};
 
-  const cvaWrapper = cva({
-    base: wrapper.base,
-    variants: wrapper.variants,
-    compoundVariants: wrapper.compoundVariants,
-  });
+  for (const key in defaultConfig) {
+    if (isSystemKey(key)) continue;
 
-  const cvaBody = cva({
-    base: body.base,
-    variants: body.variants,
-    compoundVariants: body.compoundVariants,
-  });
+    const classes = computed(() => {
+      const value = config.value[key];
 
-  const wrapperClasses = computed(() =>
-    setColor(
-      cvaWrapper({
-        color: getColor(props.color),
-        bordered: props.bordered,
-        variant: props.variant,
-      }),
-      props.color,
-    ),
-  );
+      if (value.variants || value.compoundVariants) {
+        return setColor(
+          cva(value)({
+            ...props,
+            color: getColor(props.color),
+          }),
+          props.color,
+        );
+      }
 
-  const bodyClasses = computed(() => setColor(cvaBody({ size: props.size }), props.color));
+      return "";
+    });
 
-  const wrapperAttrs = getAttrs("wrapper", { classes: wrapperClasses });
-  const bodyAttrs = getAttrs("body", { classes: bodyClasses });
-  const titleAttrs = getAttrs("title");
-  const descriptionAttrs = getAttrs("description");
-
-  const buttonAttrs = getAttrs("button", { isComponent: true });
-  const iconAttrs = getAttrs("icon", { isComponent: true });
+    attrs[`${key}Attrs`] = getAttrs(key, { classes });
+  }
 
   return {
+    ...attrs,
     config,
-    wrapperAttrs,
-    bodyAttrs,
-    buttonAttrs,
-    iconAttrs,
     hasSlotContent,
-    titleAttrs,
-    descriptionAttrs,
   };
 }
