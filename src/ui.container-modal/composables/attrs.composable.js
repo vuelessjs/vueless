d0e318252d90cb62c7b323ed28f183745dee33bf
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-07-18 14:52:12 +0300

    Update UModal's composable

--- src/ui.container-modal/composables/attrs.composable.js
+++ src/ui.container-modal/composables/attrs.composable.js
@@ -6,82 +6,59 @@ import { computed } from "vue";
 
 import useBreakpoint from "../../composable.breakpoint";
 
-export function useAttrs(props) {
-  const { config, getAttrs, hasSlotContent } = useUI(defaultConfig, () => props.config, "wrapper");
-  const { modal } = config.value;
-  const { isMobileBreakpoint } = useBreakpoint();
-
-  const cvaModal = cva({
-    base: modal.base,
-    variants: modal.variants,
-    compoundVariants: modal.compoundVariants,
-  });
-
-  const modalClasses = computed(() =>
-    cvaModal({
-      width: props.width,
-      inner: props.inner,
-    }),
+export default function useAttrs(props) {
+  const { config, getAttrs, hasSlotContent, isSystemKey } = useUI(
+    defaultConfig,
+    () => props.config,
   );
+  const { isMobileBreakpoint } = useBreakpoint();
+  const attrs = {};
 
-  const wrapperAttrs = getAttrs("wrapper");
-  const overlayAttrs = getAttrs("overlay");
-  const innerWrapperAttrs = getAttrs("innerWrapper");
-  const headerAttrsRaw = getAttrs("header");
-  const headerLeftAttrs = getAttrs("headerLeft");
-  const headerLeftFallbackAttrs = getAttrs("headerLeftFallback");
-  const backLinkAttrs = getAttrs("backLink", { isComponent: true });
-  const backLinkIconAttrs = getAttrs("backLinkIcon", { isComponent: true });
-  const titleAttrs = getAttrs("title", { isComponent: true });
-  const descriptionAttrs = getAttrs("description");
-  const headerRightAttrs = getAttrs("headerRight");
-  const closeIconAttrs = getAttrs("closeIcon", { isComponent: true });
-  const bodyAttrs = getAttrs("body");
-  const dividerAttrs = getAttrs("divider", { isComponent: true });
-  const dividerSpacingAttrs = getAttrs("dividerSpacing", { isComponent: true });
-  const footerAttrsRaw = getAttrs("footer");
+  for (const key in defaultConfig) {
+    if (isSystemKey(key)) continue;
 
-  const headerAttrs = computed(() => ({
-    ...headerAttrsRaw.value,
-    class: cx([
-      headerAttrsRaw.value.class,
-      props.mobileFooterReverse && isMobileBreakpoint.value && config.value.footerMobileReverse,
-    ]),
-  }));
+    const classes = computed(() => {
+      const value = config.value[key];
 
-  const footerAttrs = computed(() => ({
-    ...footerAttrsRaw.value,
-    class: cx([
-      footerAttrsRaw.value.class,
-      props.mobileFooterReverse && isMobileBreakpoint.value && config.value.footerMobileReverse,
-    ]),
-  }));
+      if (value.variants || value.compoundVariants) {
+        return cva(value)({
+          ...props,
+        });
+      }
 
-  const footerLeftAttrs = getAttrs("footerLeft");
-  const footerRightAttrs = getAttrs("footerRight");
-  const modalAttrs = getAttrs("modal", { classes: modalClasses });
+      return "";
+    });
+
+    attrs[`${key}Attrs`] = getAttrs(key, { classes });
+
+    if (key === "header") {
+      const headerAttrs = attrs[`${key}Attrs`];
+
+      attrs[`${key}Attrs`] = computed(() => ({
+        ...headerAttrs.value,
+        class: cx([
+          headerAttrs.value.class,
+          props.mobileFooterReverse && isMobileBreakpoint.value && config.value.footerMobileReverse,
+        ]),
+      }));
+    }
+
+    if (key === "footer") {
+      const footerAttrs = attrs[`${key}Attrs`];
+
+      attrs[`${key}Attrs`] = computed(() => ({
+        ...footerAttrs.value,
+        class: cx([
+          footerAttrs.value.class,
+          props.mobileFooterReverse && isMobileBreakpoint.value && config.value.footerMobileReverse,
+        ]),
+      }));
+    }
+  }
 
   return {
+    ...attrs,
     config,
-    modalAttrs,
-    titleAttrs,
-    backLinkAttrs,
-    backLinkIconAttrs,
-    closeIconAttrs,
-    dividerAttrs,
-    dividerSpacingAttrs,
-    overlayAttrs,
-    wrapperAttrs,
-    innerWrapperAttrs,
-    headerAttrs,
-    headerLeftAttrs,
-    headerLeftFallbackAttrs,
-    descriptionAttrs,
-    headerRightAttrs,
-    bodyAttrs,
-    footerLeftAttrs,
-    footerAttrs,
-    footerRightAttrs,
     hasSlotContent,
   };
 }
