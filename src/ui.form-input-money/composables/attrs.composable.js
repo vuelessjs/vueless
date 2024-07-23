03308f873bc17ba1eea17942a8ba3da3c272a28a
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-07-23 15:29:31 +0300

    Update UInputMoney composable's architecture and storybook

--- src/ui.form-input-money/composables/attrs.composable.js
+++ src/ui.form-input-money/composables/attrs.composable.js
@@ -1,13 +1,32 @@
 import useUI from "../../composable.ui";
-
+import { computed } from "vue";
+import { cva } from "../../service.ui";
 import defaultConfig from "../configs/default.config";
 
 export default function useAttrs(props) {
-  const { getAttrs } = useUI(defaultConfig, () => props.config);
+  const { config, getAttrs, isSystemKey } = useUI(defaultConfig, () => props.config);
+  const attrs = {};
 
-  const inputAttrs = getAttrs("input", { isComponent: true });
+  for (const key in defaultConfig) {
+    if (isSystemKey(key)) continue;
+
+    const classes = computed(() => {
+      const value = config.value[key];
+
+      if (value.variants || value.compoundVariants) {
+        return cva(value)({
+          ...props,
+        });
+      }
+
+      return "";
+    });
+
+    attrs[`${key}Attrs`] = getAttrs(key, { classes });
+  }
 
   return {
-    inputAttrs,
+    ...attrs,
+    config,
   };
 }
--- src/ui.form-input-money/configs/default.config.js
+++ src/ui.form-input-money/configs/default.config.js
@@ -1,5 +1,5 @@
 export default /*tw*/ {
-  input: "",
+  input: "{UInput}",
   defaultVariants: {
     symbol: "$",
     size: "md",
