8b23b32b7d45a8ecb18f9d38f2ae236db1ab7a01
Johnny Grid <ilevel.ws@gmail.com>
2024-08-14 23:05:29 +0800

    Merged UGroups into UGroup.

--- src/ui.container-group/configs/default.config.js
+++ src/ui.container-group/configs/default.config.js
@@ -12,8 +12,22 @@ export default /*tw*/ {
   headerFallback: "flex items-center",
   title: "{UHeader} pr-2",
   underline: "{UDivider} pt-1.5",
+  content: {
+    base: "flex flex-col items-stretch",
+    variants: {
+      gap: {
+        none: "gap-0",
+        xs: "gap-8",
+        sm: "gap-10",
+        md: "gap-12",
+        lg: "gap-14",
+        xl: "gap-16",
+      },
+    },
+  },
   defaultVariants: {
-    upperlined: true,
+    gap: "md",
+    upperlined: false,
     underlined: false,
   },
 };
