0ea802f322ac36e929099ebe7fc508b8c8633e07
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-08-19 12:19:35 +0300

    Dropdowns group composables' refactoring (isCVA)

--- src/ui.dropdown-list/composables/attrs.composable.js
+++ src/ui.dropdown-list/composables/attrs.composable.js
@@ -5,7 +5,7 @@ import defaultConfig from "../configs/default.config";
 import { computed } from "vue";
 
 export default function useAttrs(props) {
-  const { config, getAttrs, hasSlotContent, isSystemKey } = useUI(
+  const { config, getAttrs, hasSlotContent, isSystemKey, isCVA } = useUI(
     defaultConfig,
     () => props.config,
   );
@@ -21,15 +21,15 @@ export default function useAttrs(props) {
     if (isSystemKey(key)) continue;
 
     const classes = computed(() => {
-      const value = config.value[key];
+      let value = config.value[key];
 
-      if (value.variants || value.compoundVariants) {
-        return cva(value)({
+      if (isCVA(value)) {
+        value = cva(value)({
           ...props,
         });
       }
 
-      return "";
+      return value;
     });
 
     attrs[`${key}Attrs`] = getAttrs(key, { classes });
