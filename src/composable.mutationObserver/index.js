859efb910a360aea305dbcc88db93d9ab756e85b
Explicit12 <explicit1@tutanota.com>
2024-09-09 14:12:24 +0300

    - Added dynamic title for table cells witch applied when cell is overflown. - Added mutationObserver composable.
    Change content of table item (onClick on with setTimeout) to test it. Unfortunately it doesn't work when you edit content directly in devtools.

---
+++ src/composable.mutationObserver/index.js
@@ -0,0 +1,45 @@
+import { onBeforeUnmount, onMounted, toValue, watch } from "vue";
+
+export function useMutationObserver(
+  target,
+  callBack,
+  config = { childList: true, attributes: true, characterData: true },
+) {
+  const observer = new MutationObserver(callBack);
+
+  onMounted(() => {
+    if (!toValue(target)) return;
+
+    if (Array.isArray) {
+      toValue(target).forEach((element) => {
+        observer.observe(element, config);
+      });
+    } else {
+      observer.observe(toValue(target), config);
+    }
+  });
+
+  watch(
+    () => toValue(target),
+    () => {
+      if (Array.isArray) {
+        toValue(target).forEach((element) => {
+          observer.observe(element, config);
+        });
+
+        return;
+      }
+
+      observer.observe(toValue(target), config);
+    },
+    {
+      deep: true,
+    },
+  );
+
+  onBeforeUnmount(() => {
+    observer.disconnect();
+  });
+
+  return { observer };
+}
