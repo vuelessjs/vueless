818c33de7239d2fc0c9d32025c3142b3eb1522d8
Johnny Grid <ilevel.ws@gmail.com>
2024-09-21 10:09:52 +0800

    Applied new useAttrs.js architecture. Increased readability.

--- src/ui.dropdown-button/useAttrs.js
+++ src/ui.dropdown-button/useAttrs.js
@@ -1,50 +1,28 @@
+import { computed } from "vue";
 import useUI from "../composables/useUI.js";
-import { cva, cx } from "../utils/utilUI.js";
 
 import defaultConfig from "./config.js";
-import { computed } from "vue";
 
 export default function useAttrs(props, { isShownOptions }) {
-  const { config, getColor, setColor, getAttrs, hasSlotContent, isSystemKey, isCVA } = useUI(
+  const { config, getKeysAttrs, hasSlotContent, getExtendingKeysClasses } = useUI(
     defaultConfig,
     () => props.config,
   );
-  const attrs = {};
 
-  for (const key in defaultConfig) {
-    if (isSystemKey(key)) continue;
+  const extendingKeys = ["dropdownButtonActive"];
+  const extendingKeysClasses = getExtendingKeysClasses(extendingKeys);
 
-    const classes = computed(() => {
-      let value = config.value[key];
-
-      if (isCVA(value)) {
-        value = cva(value)({
-          ...props,
-          color: getColor(props.color),
-        });
-      }
-
-      return setColor(value, props.color);
-    });
-
-    attrs[`${key}Attrs`] = getAttrs(key, { classes });
-
-    if (key === "dropdownButton") {
-      const dropdownButton = attrs[`${key}Attrs`];
-
-      attrs[`${key}Attrs`] = computed(() => ({
-        ...dropdownButton.value,
-        class: cx([
-          dropdownButton.value.class,
-          isShownOptions.value && setColor(config.value.dropdownButtonActive, props.color),
-        ]),
-      }));
-    }
-  }
+  const keysAttrs = getKeysAttrs({}, extendingKeys, {
+    dropdownButton: {
+      extend: computed(() => [
+        isShownOptions.value && extendingKeysClasses.dropdownButtonActive.value,
+      ]),
+    },
+  });
 
   return {
-    ...attrs,
     config,
+    ...keysAttrs,
     hasSlotContent,
   };
 }
