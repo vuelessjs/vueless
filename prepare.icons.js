29e61a382910864d925ad11361a947d9385d6e29
Johnny Grid <ilevel.ws@gmail.com>
2024-10-10 22:31:51 +0800

    Fixed icon preparation script.

--- prepare.icons.js
+++ prepare.icons.js
@@ -1,4 +1,5 @@
 import { copyIcons } from "@vueless/plugin-vite/utils/iconLoader.js";
+import { getVueSourceFile } from "@vueless/plugin-vite/utils/common.js";
 
 /* Copy SVG icons from the default icon library into the assets' folder. */
-await copyIcons({ mode: "vuelessIcons", env: "vueless" });
+await copyIcons({ mode: "vuelessIcons", env: "vueless", targetFiles: [getVueSourceFile()] });
