d44205c0dd7b16e27b2b9f7b5d63e90a670a9bde
Explicit12 <explicit1@tutanota.com>
2024-10-11 12:40:58 +0300

    - Added override for overload which accept only content string. - Disabled tooltip when content it falsy value.

--- src/directives/vTooltip.js
+++ src/directives/vTooltip.js
@@ -25,11 +25,25 @@ export default {
   mounted(el, bindings) {
     if (isSSR) return;
 
-    tippy(el, merge(settings, bindings.value || {}));
+    if (typeof bindings.value === "string" && bindings.value.length) {
+      tippy(el, merge(settings, { content: bindings.value }));
+
+      return;
+    }
+
+    if (bindings.value.content && bindings.value.content.length) {
+      tippy(el, merge(settings, bindings.value || {}));
+    }
   },
 
   updated(el, bindings) {
     if (!el._tippy || isSSR) return;
+
+    if (typeof bindings.value === "string") {
+      el._tippy.setProps(merge(settings, { content: bindings.value }));
+
+      return;
+    }
 
     el._tippy.setProps(merge(settings, bindings.value || {}));
   },
