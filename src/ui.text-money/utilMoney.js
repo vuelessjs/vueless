b9f511e2f64e0b092a642c0c373671f84126c85a
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-10-18 19:25:44 +0300

    Update UMoney logic according to UInputMoney functionality (min-/maxFractionDigits, thousandsSeparator, etc.)

--- src/ui.text-money/utilMoney.js
+++ src/ui.text-money/utilMoney.js
@@ -8,21 +8,35 @@ export const ADD_SPACE_IN_MONEY_REG_EX = /(\d)(?=(\d{3})+(\D|$))/g;
 export const SINGLE_ZERO = "0";
 export const DOUBLE_ZERO = "00";
 
-export function separatedMoney(money, decimalPlaces = 2, decimalSeparator = ",") {
-  const roundedMoney = money ? money.toFixed(decimalPlaces) : 0;
+export function separatedMoney(
+  money,
+  minFractionDigits = 0,
+  maxFractionDigits = 2,
+  decimalSeparator = ",",
+  thousandsSeparator = " ",
+) {
+  const options = {
+    minimumFractionDigits: minFractionDigits,
+    maximumFractionDigits: maxFractionDigits,
+  };
 
-  let [integer, penny] = String(roundedMoney).split(".");
+  const formattedMoney =
+    money !== undefined && money !== null ? Number(money).toLocaleString("en-US", options) : "0";
 
-  integer = integer.replace(ADD_SPACE_IN_MONEY_REG_EX, "$1 ");
+  let [integer, penny = ""] = formattedMoney.split(".");
 
-  if (!roundedMoney) {
+  integer = integer.replace(/,/g, thousandsSeparator);
+
+  if (!money && money !== 0) {
     integer = SINGLE_ZERO;
     penny = DOUBLE_ZERO;
   }
 
-  if (decimalPlaces === 0) {
+  if (minFractionDigits === 0 || maxFractionDigits === 0) {
     decimalSeparator = "";
     penny = "";
+  } else if (penny.length < minFractionDigits) {
+    penny = penny.padEnd(minFractionDigits, "0");
   }
 
   return { integer, penny, decimalSeparator };
