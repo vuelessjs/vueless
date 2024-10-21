4bc40a779c9dd13635de3c14bfa8c64bccf80bcf
Johnny Grid <ilevel.ws@gmail.com>
2024-10-21 22:56:22 +0800

    Created vueless types.

---
+++ src/types.ts
@@ -0,0 +1,93 @@
+import UTextDefaultConfig from "./ui.text-block/config";
+import UButtonDefaultConfig from "./ui.button/config";
+
+export interface VuelessConfig {
+  /**
+   * Components brand (primary) color.
+   */
+  brand?: BrandColors;
+
+  /**
+   * Components gray (secondary) color.
+   */
+  gray?: GrayColors;
+
+  /**
+   * Default components ring width.
+   */
+  ring?: number;
+
+  /**
+   * Default components ring offset width.
+   */
+  ringOffset?: number;
+
+  /**
+   * Default components rounding (border-radius).
+   */
+  rounding?: number;
+
+  /**
+   * Default dark mode state.
+   */
+  darkMode?: boolean;
+
+  /**
+   * Component classes merge behavior.
+   * – merge (default) – smartly merge provided custom classes with default config classes.
+   * – replace – replace default config keys by provided custom keys (override only provided keys, the rest classes will be taken from the default config).
+   * – override – override default config by provided custom config (keeps only custom config, removes all default classes).
+   */
+  strategy?: Strategies;
+
+  /**
+   * Component configs.
+   */
+  component?: Partial<VuelessComponents>;
+
+  /**
+   * Tailwind-merge config extension for custom classes.
+   * All lists of rules available here:
+   * https://github.com/dcastil/tailwind-merge/blob/v2.3.0/src/lib/default-config.ts.
+   */
+  tailwindMerge?: object;
+}
+
+export type VuelessComponentNames = keyof VuelessComponents | undefined; // keys union
+export type Strategies = "merge" | "replace" | "override";
+export type GrayColors = "slate" | "cool" | "zinc" | "neutral" | "stone";
+export type BrandColors =
+  | "grayscale"
+  | "red"
+  | "orange"
+  | "amber"
+  | "yellow"
+  | "lime"
+  | "green"
+  | "emerald"
+  | "teal"
+  | "cyan"
+  | "sky"
+  | "blue"
+  | "indigo"
+  | "violet"
+  | "purple"
+  | "fuchsia"
+  | "pink"
+  | "rose";
+
+export interface VuelessComponents {
+  UText?: Partial<typeof UTextDefaultConfig>;
+  UButton?: Partial<typeof UButtonDefaultConfig>;
+}
+
+export interface VuelessCommonComponent {
+  i18n?: object;
+  defaults?: object;
+  strategy?: Strategies;
+  safelist?: object;
+  component?: string;
+  transition?: object;
+  safelistColors?: BrandColors;
+  [key: string]: object | string | undefined;
+}
