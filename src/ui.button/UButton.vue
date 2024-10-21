1a92473a47f1d94b454ee0fd6058673b424ac60e
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-10-21 19:12:05 +0300

    Roll back icons' default values

--- src/ui.button/UButton.vue
+++ src/ui.button/UButton.vue
@@ -150,7 +150,7 @@ const props = defineProps({
    */
   leftIcon: {
     type: String,
-    default: "star",
+    default: "",
   },
 
   /**
@@ -158,7 +158,7 @@ const props = defineProps({
    */
   rightIcon: {
     type: String,
-    default: "archive",
+    default: "",
   },
 
   /**
