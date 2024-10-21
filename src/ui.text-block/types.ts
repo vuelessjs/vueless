76ca53a1b13056f02e718e72e40d805f30176129
Johnny Grid <ilevel.ws@gmail.com>
2024-10-21 22:58:18 +0800

    Migrated component into ts.

---
+++ src/ui.text-block/types.ts
@@ -0,0 +1,33 @@
+import defaultConfig from "./config";
+
+export interface UTextProps {
+  /**
+   * HTML markdown or plain text.
+   */
+  html?: string;
+
+  /**
+   * Text size.
+   */
+  size?: "sm" | "md" | "lg";
+
+  /**
+   * Text align.
+   */
+  align?: "left" | "center" | "right";
+
+  /**
+   * Remove line height (useful for 1-line text).
+   */
+  line?: boolean;
+
+  /**
+   * Component config object.
+   */
+  config?: Partial<typeof defaultConfig>;
+
+  /**
+   * Data-test attribute for automated testing.
+   */
+  dataTest?: string;
+}
