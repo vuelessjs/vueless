971bee460a1315daab8c728e549f9c3fd2b4cbc2
Johnny Grid <ilevel.ws@gmail.com>
2024-10-22 14:47:47 +0800

    Fixed ts compiler issues.

--- src/ui.text-block/useAttrs.ts
+++ src/ui.text-block/useAttrs.ts
@@ -2,15 +2,16 @@ import useUI from "../composablesTs/useUI";
 
 import defaultConfig from "./config";
 import type { UTextProps } from "./types";
+import type { VuelessAttrs } from "../types";
 
 export default function useAttrs(props: UTextProps) {
   const { config, getKeysAttrs, hasSlotContent } = useUI(defaultConfig, () => props.config);
 
-  const keysAttrs: any = getKeysAttrs();
+  const keysAttrs = getKeysAttrs();
 
   return {
     config,
     ...keysAttrs,
     hasSlotContent,
-  };
+  } as VuelessAttrs;
 }
