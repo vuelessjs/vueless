9628e9fa398da03e9e1809224e3c0984a4045660
Johnny Grid <ilevel.ws@gmail.com>
2024-11-02 13:47:11 +0800

    Added d.ts file for vite plugin

---
+++ src/vite.d.ts
@@ -0,0 +1,10 @@
+export function VuelessUnpluginComponents(options?: unknown): import("vite").Plugin<unknown> & {
+  api: import("unplugin-vue-components/types.js").PublicPluginAPI;
+};
+
+export function Vueless(options?: {
+  env?: string;
+  mode?: string;
+  debug?: boolean;
+  include?: string[];
+}): never;
