48f871ba72a1ae0d99c61c594a4bac483ca448a2
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-11-14 10:30:31 +0200

    Add eslint no-console disable rule for UDropdownList story

--- src/ui.dropdown-list/storybook/stories.ts
+++ src/ui.dropdown-list/storybook/stories.ts
@@ -98,6 +98,7 @@ OptionSettings.args = {
     {
       label: "option 3",
       id: "3",
+      // eslint-disable-next-line no-console
       onClick: (option: object) => console.log("onClick option 3 ", option),
     },
   ],
