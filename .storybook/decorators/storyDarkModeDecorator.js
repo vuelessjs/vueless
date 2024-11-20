60cb25a090729d1f69b8dc458ed884010a6cdf10
Johnny Grid <ilevel.ws@gmail.com>
2024-11-21 00:04:25 +0800

    Temporary disabled auto dark mode from the production.

--- .storybook/decorators/storyDarkModeDecorator.js
+++ .storybook/decorators/storyDarkModeDecorator.js
@@ -1,12 +1,13 @@
 import { DecoratorHelpers } from "@storybook/addon-themes";
 import { setTheme } from "../../src/index.ts";
+import { COLOR_MODE_KEY } from "../../src/constants.js";
 
 const { initializeThemeState, pluckThemeFromContext } = DecoratorHelpers;
 
 export const storyDarkModeDecorator = () => {
   const prefersColorSchemeDark = window.matchMedia("(prefers-color-scheme: dark)");
   const preferScheme = prefersColorSchemeDark.matches ? "dark" : "light";
-  const cachedColorMode = localStorage.getItem("vl-color-mode") || preferScheme;
+  const cachedColorMode = localStorage.getItem(COLOR_MODE_KEY) || preferScheme;
 
   initializeThemeState(["light", "dark"], cachedColorMode);
 
@@ -14,7 +15,8 @@ export const storyDarkModeDecorator = () => {
     const theme = pluckThemeFromContext(context);
 
     setTheme({
-      colorMode: theme,
+      // TODO: Remove this condition when all component will have dark classes
+      colorMode: process.env.NODE_ENV !== "development" ? theme : "light",
     });
 
     return {
