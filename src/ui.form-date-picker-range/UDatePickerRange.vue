db7d965557ea36ffb183a409a28f5a1e8a6562fd
Explicit12 <explicit1@tutanota.com>
2024-11-21 10:03:03 +0200

    Fixed datepicker range previous date button

--- src/ui.form-date-picker-range/UDatePickerRange.vue
+++ src/ui.form-date-picker-range/UDatePickerRange.vue
@@ -458,8 +458,8 @@ function shiftRangePrev(to: Date, from: Date, daysDifference: number) {
     if (isDatePeriodOutOfRange(previousDate)) return;
 
     localValue.value = {
-      from: previousDate.startRange,
       to: previousDate.startRange,
+      from: previousDate.endRange,
     };
   } else {
     const reverseDatesList = [...periodDateList.value].reverse();
