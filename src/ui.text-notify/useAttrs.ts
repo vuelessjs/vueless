61eb9480640c06ff662c2d2ca2b7df2c0f52b661
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-12-02 12:16:47 +0200

    Fix lint error

--- src/ui.text-notify/useAttrs.ts
+++ src/ui.text-notify/useAttrs.ts
@@ -2,23 +2,11 @@ import useUI from "../composables/useUI.ts";
 
 import defaultConfig from "./config.ts";
 
-import type { Ref } from "vue";
 import type { UseAttrs } from "../types.ts";
 import type { UNotifyProps, Config } from "./types.ts";
 
-type ComponentState = {
-  notifications: Ref<{ type: string }[]>;
-};
-
-export default function useAttrs(
-  props: UNotifyProps,
-  { notifications }: ComponentState,
-): UseAttrs<Config> {
-  const { config, getKeysAttrs } = useUI<Config>(
-    defaultConfig,
-    () => props.config,
-  );
-
+export default function useAttrs(props: UNotifyProps): UseAttrs<Config> {
+  const { config, getKeysAttrs } = useUI<Config>(defaultConfig, () => props.config);
 
   return { config, ...getKeysAttrs() };
 }
