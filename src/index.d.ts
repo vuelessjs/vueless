1f1875d051441b837d9030ddd70d7f394c8f8154
Johnny Grid <ilevel.ws@gmail.com>
2024-12-25 23:21:08 +0800

    Added needed exports for custom component development.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,7 @@
 import type { App } from "vue";
 import type { CreateVuelessOptions } from "./types.ts";
 export { setTheme } from "./utils/theme.ts";
-export { cx, cva, compose } from "./utils/ui.ts";
+export { cx, cva, compose, getDefaults } from "./utils/ui.ts";
 export {
   isSSR,
   isCSR,
@@ -10,14 +10,16 @@ export {
   createDebounce,
   hasSlotContent,
 } from "./utils/helper.ts";
+export { getArgTypes, getSlotNames, getSlotsFragment, getSource } from "./utils/storybook.ts";
 export { isMac, isPWA, isIOS, isAndroid, isMobileApp, isWindows } from "./utils/platform.ts";
-export { default as createVueI18nAdapter } from "./adatper.locale/vue-i18n.js";
-export { default as defaultEnLocale } from "./adatper.locale/locales/en.js";
+export { default as createVueI18nAdapter } from "./adatper.locale/vue-i18n.ts";
+export { default as defaultEnLocale } from "./adatper.locale/locales/en.ts";
+export { default as useUI } from "./composables/useUI.ts";
 export { useLocale } from "./composables/useLocale.ts";
-export { useLoaderProgress } from "./ui.loader-progress/useLoaderProgress.js";
-export { loaderProgressOn, loaderProgressOff } from "./ui.loader-progress/utilLoaderProgress.js";
-export { useLoaderOverlay } from "./ui.loader-overlay/useLoaderOverlay.js";
-export { loaderOverlayOn, loaderOverlayOff } from "./ui.loader-overlay/utilLoaderOverlay.js";
+export { useLoaderProgress } from "./ui.loader-progress/useLoaderProgress.ts";
+export { loaderProgressOn, loaderProgressOff } from "./ui.loader-progress/utilLoaderProgress.ts";
+export { useLoaderOverlay } from "./ui.loader-overlay/useLoaderOverlay.ts";
+export { loaderOverlayOn, loaderOverlayOff } from "./ui.loader-overlay/utilLoaderOverlay.ts";
 export {
   notify,
   notifySuccess,
@@ -26,7 +28,7 @@ export {
   clearNotifications,
   setDelayedNotify,
   getDelayedNotify,
-} from "./ui.text-notify/utilNotify.js";
+} from "./ui.text-notify/utilNotify.ts";
 export declare function createVueless(options?: CreateVuelessOptions): {
   install: (app: App) => void;
 };
