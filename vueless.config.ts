31e74b9d0a7e37d818732faace12dfe35e7e333d
Johnny Grid <ilevel.ws@gmail.com>
2024-12-27 12:57:53 +0800

    Added comments into the main config.

--- vueless.config.ts
+++ vueless.config.ts
@@ -1,7 +1,11 @@
 export default {
+  /**
+   * Global settings.
+   */
   strategy: "merge",
   brand: "grayscale",
   gray: "cool",
+  darkMode: "auto",
   ring: 4,
   ringOffset: 0,
   ringOffsetColorLight: "#ffffff", // white
@@ -9,12 +13,21 @@ export default {
   roundingSm: 4,
   rounding: 8,
   roundingLg: 16,
-  darkMode: "auto",
-  directive: {
-    // directive configs
-  },
-  component: /*tw*/ {
-    // component configs
-  },
+
+  /**
+   * Directive settings.
+   */
+  directive: {},
+
+  /**
+   * Component settings.
+   */
+  component: /*tw*/ {},
+
+  /**
+   * Custom classes TailwindMerge settings.
+   * All lists of rules available here:
+   * https://github.com/dcastil/tailwind-merge/blob/v2.3.0/src/lib/default-config.ts
+   */
   tailwindMerge: {},
 };
