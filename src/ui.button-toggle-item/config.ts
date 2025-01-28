8d1e0031a6efd025988c59b5cb9292aff3194b6b
Johnny Grid <ilevel.ws@gmail.com>
2025-01-28 14:03:21 +0800

    Improved styles.

--- src/ui.button-toggle-item/config.ts
+++ src/ui.button-toggle-item/config.ts
@@ -5,9 +5,29 @@ export default /*tw*/ {
       variant: "thirdary",
     },
   },
-  toggleButtonInactive: "{>toggleButton}",
+  toggleButtonInactive: {
+    base: "{>toggleButton}",
+    variants: {
+      split: {
+        false: "border-0",
+        true: `
+          border border-gray-300 hover:border-gray-400
+          hover:bg-transparent dark:hover:bg-transparent
+          active:bg-transparent dark:active:bg-transparent
+        `,
+      },
+    },
+  },
   toggleButtonActive: {
     base: "{>toggleButton}",
+    variants: {
+      split: {
+        false: "border-0",
+        true: `
+          border border-brand-600
+        `,
+      },
+    },
     defaults: {
       color: "brand",
       filled: true,
