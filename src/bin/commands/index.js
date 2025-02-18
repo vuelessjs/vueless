98f1b32d78968d1bd9fa8458ea30485e348fdbd9
Johnny Grid <ilevel.ws@gmail.com>
2025-02-18 22:58:20 +0800

    Fixed init cli command.

--- src/bin/commands/index.js
+++ src/bin/commands/index.js
@@ -1,9 +1,9 @@
-import { vuelssInit } from "./init.js";
+import { vuelessInit } from "./init.js";
 import { createVuelessComponent } from "./create.js";
 import { copyVuelessComponent } from "./copy.js";
 
 export const commands = {
-  init: vuelssInit,
+  init: vuelessInit,
   create: createVuelessComponent,
   copy: copyVuelessComponent,
 };
