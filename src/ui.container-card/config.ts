3ba0065a086e428a0d21d9e19c281207cd46b450
Vitalii Dudnik <dudnikvtl@gmail.com>
2025-03-13 16:32:27 +0200

    Add text-medium on wrapper

--- src/ui.container-card/config.ts
+++ src/ui.container-card/config.ts
@@ -1,10 +1,10 @@
 export default /*tw*/ {
-  wrapper: "p-4 md:p-5 border rounded-large border-muted bg-default space-y-4 w-full",
+  wrapper: "p-4 md:p-5 border rounded-large border-muted bg-default space-y-4 w-full text-medium",
   header: "flex justify-between",
   headerLeft: "flex items-center gap-2 w-fit",
   headerLeftFallback: "flex flex-col w-fit",
   title: "{UHeader}",
-  description: "mt-1 text-medium font-normal text-lifted",
+  description: "mt-1 font-normal text-lifted",
   cardDivider: "{UDivider}",
   footer: "flex justify-between w-full",
   footerLeft: "",
