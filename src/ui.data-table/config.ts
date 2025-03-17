6726fea8f880bc5ab0910017c7088f4210a9688a
Vitalii Dudnik <dudnikvtl@gmail.com>
2025-03-17 11:44:24 +0200

    Change class condition in config

--- src/ui.data-table/config.ts
+++ src/ui.data-table/config.ts
@@ -2,13 +2,13 @@ export default /*tw*/ {
   wrapper: "relative w-full overflow-auto",
   headerCounterBase: "mr-1.5 pr-1.5 font-medium text-medium",
   stickyHeader: {
-    base: "top-0 flex items-center z-30 overflow-hidden border rounded-none",
+    base: "fixed top-0 flex items-center z-30 overflow-hidden border rounded-none",
     variants: {
-      actionsHeader: {
-        true: "absolute rounded-t-medium border-muted bg-muted",
-      },
       stickedHeader: {
-        true: "fixed",
+        false: "absolute",
+      },
+      actionsHeader: {
+        true: "rounded-t-medium border-muted bg-muted",
       },
     },
     compoundVariants: [
