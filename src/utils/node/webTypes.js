ef140c8d40d378d6da5126a2e509878625b03851
Johnny Grid <ilevel.ws@gmail.com>
2025-03-29 22:08:01 +0800

    Fixed web-types script generator import path.

--- src/utils/node/webTypes.js
+++ src/utils/node/webTypes.js
@@ -2,7 +2,7 @@ import { vuelessConfig } from "./vuelessConfig.js";
 
 export async function buildWebTypes() {
   try {
-    const { default: build } = await import("@vueless/storybook/webTypes");
+    const { default: build } = await import("@vueless/storybook/webTypes/index.js");
 
     await build(vuelessConfig);
   } catch (error) {
