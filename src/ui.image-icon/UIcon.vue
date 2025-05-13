edb0c1787684975e8205999b0be702e3df643370
Johnny Grid <ilevel.ws@gmail.com>
2025-05-13 23:32:19 +0800

    Refactor UIcon to simplify icon imports and remove redundancy
    Simplified the dynamic icon import logic by consolidating redundant code paths and removing unused variables.

--- src/ui.image-icon/UIcon.vue
+++ src/ui.image-icon/UIcon.vue
@@ -3,7 +3,6 @@ import { computed, defineAsyncComponent, useTemplateRef } from "vue";
 
 import useUI from "../composables/useUI.ts";
 import { getDefaults } from "../utils/ui.ts";
-import { isSSR } from "../utils/helper.ts";
 import { APP_ICONS_CACHED_DIR, VUELESS_LIBRARY } from "../constants.js";
 
 import { COMPONENT_NAME } from "./constants.ts";
@@ -30,7 +29,8 @@ const iconRef = useTemplateRef<ComponentPublicInstance | HTMLElement>("icon");
 const generatedIcons = computed(() => {
   return (
     Object.entries(
-      import.meta.glob(`/node_modules/.cache/vueless/icons/app/**/*.svg`, {
+      // TODO: Fix path to import `app` related icons only
+      import.meta.glob(`/node_modules/.cache/vueless/icons/**/*.svg`, {
         eager: true,
         query: "?component",
       }),
@@ -39,7 +39,6 @@ const generatedIcons = computed(() => {
 });
 
 const dynamicComponent = computed(() => {
-  const FILL_SUFFIX = "-fill";
   const ICON_EXTENSION = ".svg";
 
   const userLibrary = config.value.defaults.library as IconLibraries;
@@ -59,10 +58,6 @@ const dynamicComponent = computed(() => {
   const isInternalIcon = isInternalIconExists && !isExternalIconExists;
 
   const library = props.internal && isInternalIcon ? VUELESS_LIBRARY : userLibrary;
-  const customLibraryPath = config.value.defaults.path;
-  const weight = config.value.defaults.weight;
-  const style = config.value.defaults.style;
-  const isFill = props.name?.endsWith(FILL_SUFFIX);
   const name = props.name;
   const src = props.src;
 
@@ -77,58 +72,12 @@ const dynamicComponent = computed(() => {
   /* Dynamic import */
   if (!name) return "";
 
-  function getIcon(name: string, params: (string | number)[] = []) {
-    const [, component] =
-      generatedIcons.value.find(([path]) =>
-        [name + ICON_EXTENSION, ...params].every((param) => path.includes(String(param))),
-      ) || [];
+  const [, component] =
+    generatedIcons.value.find(([path]) =>
+      [name + ICON_EXTENSION, library].every((param) => path.includes(String(param))),
+    ) || [];
 
-    return component;
-  }
-
-  /* eslint-disable prettier/prettier */
-  const libraries = {
-    "vueless": async () => {
-      return import.meta.env.PROD
-        ? await getIcon(name)
-        : isSSR
-          ? import(/* @vite-ignore */ `${APP_ICONS_CACHED_DIR}/${VUELESS_LIBRARY}/${name}.svg?component`)
-          : import(/* @vite-ignore */ `/${APP_ICONS_CACHED_DIR}/${VUELESS_LIBRARY}/${name}.svg?component`);
-    },
-    "@material-symbols": async () => {
-      return import.meta.env.PROD
-        ? await getIcon(name, [library, weight, style])
-        : isSSR
-          ? import(/* @vite-ignore */ `node_modules/${library}/svg-${weight}/${style}/${name}.svg?component`)
-          : import(/* @vite-ignore */ `/node_modules/${library}/svg-${weight}/${style}/${name}.svg?component`);
-    },
-    "bootstrap-icons": async () => {
-      return import.meta.env.PROD
-        ? await getIcon(name, [library])
-        : isSSR
-          ? import(/* @vite-ignore */ `node_modules/${library}/icons/${name}.svg?component`)
-          : import(/* @vite-ignore */ `/node_modules/${library}/icons/${name}.svg?component`);
-    },
-    "heroicons": async () => {
-      const fillType = isFill ? "solid" : "outline";
-
-      return import.meta.env.PROD
-        ? await getIcon(name, [library, fillType])
-        : isSSR
-          ? import(/* @vite-ignore */ `node_modules/${library}/24/${fillType}/${name}.svg?component`)
-          : import(/* @vite-ignore */ `/node_modules/${library}/24/${fillType}/${name}.svg?component`);
-    },
-    "custom-icons": async () => {
-      return import.meta.env.PROD
-        ? await getIcon(name, [library])
-        : isSSR
-          ? import(/* @vite-ignore */ `${customLibraryPath}/${name}.svg?component`)
-          : import(/* @vite-ignore */ `/${customLibraryPath}/${name}.svg?component`);
-    },
-  };
-  /* eslint-enable prettier/prettier */
-
-  return defineAsyncComponent(libraries[library] as AsyncComponentLoader);
+  return defineAsyncComponent(async () => (await component) as AsyncComponentLoader);
 });
 
 function onClick(event: MouseEvent) {
