080eebc5c63f744427e12af7c95a2ecac4fbfef5
Johnny Grid <ilevel.ws@gmail.com>
2025-05-21 16:42:22 +0800

    Refactor import order and handle TypeScript declaration issue.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vitest/config";
 import Vue from "@vitejs/plugin-vue";
 
-import { Vueless, TailwindCSS } from "./src/plugin-vite";
+/* @ts-expect-error: TypeScript cannot find the declaration. */
 import { INTERNAL_ENV } from "./src/constants.js";
+import { Vueless, TailwindCSS } from "./src/plugin-vite";
 
 export default defineConfig({
   plugins: [Vue(), Vueless({ env: INTERNAL_ENV }), TailwindCSS()],
