7752474909653fbb1d4b1d1b0dde6f74e1b44653
Explicit12 <explicit1@tutanota.com>
2025-05-28 09:59:07 +0300

    Removed useLocaleTm composable

--- src/composables/useComponentLocaleMassages.ts
+++ src/composables/useComponentLocaleMassages.ts
@@ -1,15 +1,21 @@
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 import { merge } from "lodash-es";
 
 import { COMPONENTS } from "../constants";
-import { useLocaleTm } from "./useLocaleTm";
+import { useLocale } from "./useLocale.ts";
 
 export function useComponentLocaleMessages<TLocale>(
   componentName: keyof typeof COMPONENTS,
   defaultLocale: Record<string, unknown>,
   propsLocale: Record<string, unknown> = {},
 ) {
-  const { messages: globalComponentMassages } = useLocaleTm(componentName);
+  const { tm, locale } = useLocale();
+
+  const globalComponentMassages = ref(tm(componentName));
+
+  watch(locale, () => {
+    globalComponentMassages.value = tm(componentName);
+  });
 
   const localeMessages = computed(
     () => merge({}, defaultLocale, globalComponentMassages.value, propsLocale) as TLocale,
