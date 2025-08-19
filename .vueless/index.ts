2eb5dac269ae4db7c7e7c409a98985fccc77ddf8
Johnny Grid <ilevel.ws@gmail.com>
2025-08-19 19:25:42 +0800

    Comment update

--- .vueless/index.ts
+++ .vueless/index.ts
@@ -4,9 +4,9 @@
  * ⚠️ This file is auto-generated — do not edit it manually.
  * It gets updated automatically whenever the Vite server restarts.
  *
- * This file imports all component config files from the current directory.
- * Only files following the naming pattern "U[Component].config.[ts|js]" will be included.
- * Example: "UButton.config.ts"
+ * This file imports all config files from the current directory and subdirectories.
+ * Only files following the naming pattern "U[Component].[ts|js]" will be included.
+ * Example: "UButton.ts"
  */
 
 import UButton from "./configs/UButton.ts";
