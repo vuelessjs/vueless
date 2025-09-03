0817206b2aafc566b782e8cde3d25b322ac5c126
Vitalii Dudnik <dudnikvtl@gmail.com>
2025-09-03 15:03:54 +0300

    Small style fix

--- src/ui.container-accordion/config.ts
+++ src/ui.container-accordion/config.ts
@@ -1,5 +1,5 @@
 export default /*tw*/ {
-  accordion: "flex flex-col w-full divide-y-[1px] divide-neutral-800",
+  accordion: "flex flex-col w-full divide-y divide-muted",
   accordionItem: "{UAccordionItem} py-5 first:pt-0 last:pb-0",
   defaults: {
     size: "md",
