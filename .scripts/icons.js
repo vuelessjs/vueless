12655baabfb71e764f6c4c96192b44e0821d78bf
Johnny Grid <ilevel.ws@gmail.com>
2025-09-10 18:12:05 +0800

    Fixed the merged config caching in `.scripts`

--- .scripts/icons.js
+++ .scripts/icons.js
@@ -11,7 +11,7 @@ import { getVueDirs, cacheMergedConfigs } from "../src/utils/node/helper.js";
 import { INTERNAL_ENV, VUELESS_LOCAL_DIR } from "../src/constants.js";
 
 /* Merge and cache component configs. */
-await cacheMergedConfigs(VUELESS_LOCAL_DIR);
+await cacheMergedConfigs({ vuelessSrcDir: VUELESS_LOCAL_DIR, basePath: "" });
 
 /* Copy SVG icons from the default icon library into the `./src/icons` folder. */
 await removeIconsCache(VUELESS_LOCAL_DIR, true);
