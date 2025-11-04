fb262e35cd78e2fab11b8926d27c7c2387f2b5a8
Johnny Grid <ilevel.ws@gmail.com>
2025-11-04 20:13:35 +0800

    feat: USplitter: add new component and update exports

--- src/components.d.ts
+++ src/components.d.ts
@@ -50,6 +50,7 @@ export { default as UCard } from "./ui.container-card/UCard.vue";
 export { default as UModal } from "./ui.container-modal/UModal.vue";
 export { default as UModalConfirm } from "./ui.container-modal-confirm/UModalConfirm.vue";
 export { default as UDrawer } from "./ui.container-drawer/UDrawer.vue";
+export { default as USplitter } from "./ui.container-splitter/USplitter.vue";
 export { default as UPage } from "./ui.container-page/UPage.vue";
 /* Images and Icons */
 export { default as UIcon } from "./ui.image-icon/UIcon.vue";
