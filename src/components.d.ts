c7b6b0e0b536deb52c83622a563bb2d9f841797f
Johnny Grid <ilevel.ws@gmail.com>
2025-11-24 17:53:10 +0800

    refactor: migrate `UPlaceholder` to `ui.container-placeholder` directory, update imports, and adjust related configs and stories

--- src/components.d.ts
+++ src/components.d.ts
@@ -46,7 +46,7 @@ export { default as UGroups } from "./ui.container-groups/UGroups.vue";
 export { default as UAccordion } from "./ui.container-accordion/UAccordion.vue";
 export { default as UAccordionItem } from "./ui.container-accordion-item/UAccordionItem.vue";
 export { default as UEmpty } from "./ui.text-empty/UEmpty.vue";
-export { default as UPlaceholder } from "./ui.other-placeholder/UPlaceholder.vue";
+export { default as UPlaceholder } from "./ui.container-placeholder/UPlaceholder.vue";
 export { default as UCard } from "./ui.container-card/UCard.vue";
 export { default as UModal } from "./ui.container-modal/UModal.vue";
 export { default as UModalConfirm } from "./ui.container-modal-confirm/UModalConfirm.vue";
