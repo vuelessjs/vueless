cc0aa9d4666dffeaf046894c9d2c2c033e3767c1
Johnny Grid <ilevel.ws@gmail.com>
2026-02-10 23:37:38 +0800

    fix: USelect: move selected item slots for multiple selected options inside custom blocks to simplify customization

--- src/ui.form-select/config.ts
+++ src/ui.form-select/config.ts
@@ -131,7 +131,7 @@ export default /*tw*/ {
     },
   },
   listbox: `
-    {UListbox} border-0 outline-1 outline-default
+    {UListbox} border-0 outline-1! outline-default
     my-1.5 group-[*]/top:bottom-full group-[*]/top:top-auto top-full w-full
   `,
   i18n: {
