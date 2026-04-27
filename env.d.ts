4f1e0fe4cd6b532f1d66807d97aa72c7a823d011
Vitalii Dudnik <dudnikvtl@gmail.com>
2026-04-27 20:53:24 +0300

    chore: update lodash types and dependencies, modify TypeScript configuration, and adjust Storybook Vite config for improved module resolution

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="./src/modules" />
+
+declare module "lodash-es" {
+  import cloneDeep from "lodash/cloneDeep";
+  import isEqual from "lodash/isEqual";
+  import merge from "lodash/merge";
+  import range from "lodash/range";
+  export { cloneDeep, isEqual, merge, range };
+}
