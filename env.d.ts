295fe8131b8910aeddd9f561f504c74393c6fd60
Vitalii Dudnik <dudnikvtl@gmail.com>
2026-04-28 12:52:01 +0300

    chore: update lodash types to lodash-es, add INTERNAL_ENV constant in env.d.ts, and include env.d.ts in Storybook tsconfig for improved type definitions

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="./src/modules" />
 
-declare module "lodash-es" {
-  import cloneDeep from "lodash/cloneDeep";
-  import isEqual from "lodash/isEqual";
-  import merge from "lodash/merge";
-  import range from "lodash/range";
-  export { cloneDeep, isEqual, merge, range };
+declare module "vueless/constants.js" {
+  export const INTERNAL_ENV: "internal";
 }
